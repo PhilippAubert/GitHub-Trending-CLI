81a832459461fdf93bc18c7f45045d7451f099da
ujin22 <p.b.m.aubert@gmail.com>
2026-01-30 18:52:59 +0100

    Get result array with potential data, remove types.ts from dist, deactivate nodemon compiling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,39 @@
 #!/usr/bin/env node
 
-console.log("HELLO WORLD!");
+import axios, { type AxiosResponse } from "axios";
+import dotenv from "dotenv";
+import type { Repo } from "./types.js";
 
+dotenv.config();
+
+const getData = async ():Promise<AxiosResponse<any, any, {}>> => {
+    const data = await axios({
+        method:"get",
+        url: "https://api.github.com/repositories"
+    })
+    return data;
+};
+
+const parseData = (repos:AxiosResponse<any, any, {}>):Array<Repo> => {
+    return repos.data.map((repo:Repo): Array<Repo> | void => {
+        repo.full_name,
+        repo.description,  
+        repo.url,
+        repo.stargazers_url,
+        repo.languages_url
+    })
+};
+
+try {
+    const repos = await getData();
+    const dataToReturn = parseData(repos);
+
+    console.log(dataToReturn);
+} catch (e:any) {
+    if (e.status === 403) {
+        console.log("You're blocked for a while yo");
+    }
+    if (e.status === 404) {
+        console.log("Can't find that");
+    }
+}
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export type Repo = {
+    full_name: string,
+    description: string,
+    url: string,
+    stargazers_url:string,
+    languages_url: any
+};
